a8eb4bdf9692b15ede3eb60435517faa35347041
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-02-09 23:49:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App items-center m-auto text-blue-800 font-bold text-center">
+      <h1 className='text-4xl pt-32'>WELCOME TO D'CLINIK</h1>
+    </div>
+  )
+}
+
+export default App
