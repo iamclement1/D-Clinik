f561de61efa5fbabb0e0f448435672637dbb0799
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-02-24 19:53:40 +0100

    created pages folder and admin sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
+import AdminSideBar from './Components/Admin/AdminSideBar'
 import logo from './logo.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App items-center m-auto text-blue-800 font-bold text-center">
-      <h1 className='text-4xl pt-32'>WELCOME TO D'CLINIK</h1>
+    <div className="App flex flex-col items-center justify-center min-h-screen py-2">
+      <AdminSideBar/>
     </div>
   )
 }
