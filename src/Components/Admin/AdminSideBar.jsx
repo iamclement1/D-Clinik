5d38e0eab564afd733f3c20f650da57c3009c6ed
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-02-24 21:49:28 +0100

    changed project title

--- src/Components/Admin/AdminSideBar.jsx
+++ src/Components/Admin/AdminSideBar.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function AdminSideBar() {
+
+    const [showSidebar, setShowSidebar ] = useState(false);
     return (
     <div className='top-0 left-0 w-[20vw] bg-blue-700 p-10 pl-5
     text-white fixed h-full'>
         <h1 className='m'>I am a side bar</h1>
     </div>
-  )
+
+)
 }
 
 export default AdminSideBar
