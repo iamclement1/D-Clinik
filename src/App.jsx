321bad34668d975bcd65bae726ebc2a97e5d04a8
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-02-25 12:02:55 +0100

    moved sidebar to right

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="App flex flex-col items-center justify-center min-h-screen">
       <h2 className='text-center font-bold text-3xl text-blue-800'>Welcome to D'Clinik</h2>
       <AdminSideBar/>
     </div>
--- src/Components/Admin/AdminSideBar.jsx
+++ src/Components/Admin/AdminSideBar.jsx
@@ -7,15 +7,15 @@ function AdminSideBar() {
     return (
 
         <>
-        { isOpen ? (
+        { !isOpen ? (
             <button className='flex text-4xl text-white items-center cursor-pointer
-            fixed left-10 top-6 z-50'
+            fixed right-10 top-6 z-50'
             onClick={() => setIsOpen(!isOpen)}>
                 x
             </button>
         ) : (
             <svg onClick={() => setIsOpen (!isOpen)}
-            className='fixed z-30 flex items-center cursor-pointer left-10 top-6'
+            className='fixed z-30 flex items-center cursor-pointer right-10 top-6'
             fill='#2563eb'
             viewBox='0 0 100 80'
             width={40}
@@ -26,10 +26,12 @@ function AdminSideBar() {
             </svg>
         )}
 
-        <div className= {`top-0 left-0 w-[20vw] bg-blue-600 pt-20 p-3 text-white
-        fixed h-full z-40 ease-in-out duration-300
-        ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-            <h2>I am a AdminSideBar</h2>
+        <div className={`top-0 right-0 w-[20vw] bg-blue-600
+        text-white fixed h-full z-40 ease-in-out duration-300
+        ${ !isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+            <h2 className='mt-20 text-sm font-semibold text-white'>
+                I am a AdminSideBar
+            </h2>
         </div>
         </>
 )
