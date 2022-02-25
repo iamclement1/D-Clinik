5a660ecf2c998820a4f3116a0f124fa28a8bdb52
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-02-25 12:40:02 +0100

    moved sidebar to right

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import AdminSideBar from './Components/Admin/AdminSideBar'
-import logo from './logo.svg'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App flex flex-col items-center justify-center min-h-screen">
-      <h2 className='text-center font-bold text-3xl text-blue-800'>Welcome to D'Clinik</h2>
-      <AdminSideBar/>
-    </div>
+    <Router>
+      <div className='App'>
+        <AdminSideBar />
+        <Routes>
+          <Route />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
--- src/Components/Admin/AdminSideBar.jsx
+++ src/Components/Admin/AdminSideBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 function AdminSideBar() {
 
@@ -8,7 +9,7 @@ function AdminSideBar() {
 
         <>
         { !isOpen ? (
-            <button className='flex text-4xl text-white items-center cursor-pointer
+            <button className='flex text-xl text-white items-center cursor-pointer
             fixed right-10 top-6 z-50'
             onClick={() => setIsOpen(!isOpen)}>
                 x
@@ -18,20 +19,29 @@ function AdminSideBar() {
             className='fixed z-30 flex items-center cursor-pointer right-10 top-6'
             fill='#2563eb'
             viewBox='0 0 100 80'
-            width={40}
-            height={40}>
+            width={20}
+            height={20}>
                 <rect width={100} height={10}></rect>
                 <rect y={30} width={100} height={10}></rect>
                 <rect y={60} width={100} height={10}></rect>
             </svg>
         )}
 
-        <div className={`top-0 right-0 w-[20vw] bg-blue-600
-        text-white fixed h-full z-40 ease-in-out duration-300
+        <div className={`top-0 right-0 w-[15vw] bg-blue-800
+        text-white fixed h-full z-40 p-10 ease-in-out duration-300
         ${ !isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-            <h2 className='mt-20 text-sm font-semibold text-white'>
+            {/* <h2 className='mt-20 text-sm font-semibold text-white'>
                 I am a AdminSideBar
-            </h2>
+            </h2> */}
+            <ul className='flex flex-col'>
+                <NavLink to='/' className='text-xl font-bold'>Home</NavLink>
+                <NavLink to='/' className='text-xl font-bold'>Home</NavLink>
+                <NavLink to='/' className='text-xl font-bold'>Home</NavLink>
+                <NavLink to='/' className='text-xl font-bold'>Home</NavLink>
+                <NavLink to='/' className='text-xl font-bold'>Home</NavLink>
+                <NavLink to='/' className='text-xl font-bold'>Home</NavLink>
+                <NavLink to='/' className='text-xl font-bold'>Home</NavLink>
+            </ul>
         </div>
         </>
 )
