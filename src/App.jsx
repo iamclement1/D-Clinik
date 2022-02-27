adeb5d3e8fcfc04b7471fa61ddb9750881827687
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-02-27 17:59:24 +0100

    changed login icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import "bootstrap/dist/css/bootstrap.min.css"
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import AdminNavbar from './Components/Admin/AdminNavbar'
 import AdminSideBar from './Components/Admin/AdminSideBar'
--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -2,23 +2,34 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { faStethoscope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import bg from '../assets/bg.jpeg'
 
 function LogIn() {
   return (
-    <div className="container justify-center">
-        <div className="row " style={{marginTop:"8rem"}}>
-            <div className="col-md-6 offset-md-3 bg-light shadow-lg px-10 py-10">
-                <form className='flex flex-col mx-auto'>
-                    <FontAwesomeIcon icon={faStethoscope} className='text-6xl text-blue-900' />
-                    <h2 className='text-4xl font-bold text-center'>Log in to your account</h2>
+    <div className=" container m-auto justify-center">
+        <div className="row">
+            <div className=" flex offset-md-3 bg-light shadow-lg
+            m-auto md:w-3/6 md:mt-10 mt-5  md:px-10 px-3 py-10 justify-cente">
+                <form className='flex flex-col mx-auto md:w-4/5 w-
+                p-5'>
+                    <FontAwesomeIcon icon={faStethoscope} className='md:text-4xl 
+                    text-2xl mb-4 text-blue-900' />
+                    <h2 className='md:text-2xl text-xl font-bold text-center'>
+                        Log in to your account
+                        
+                    </h2>
                     <div className="mb-3 mt-3">
-                        <label for="email" className="form-label">Email:</label>
-                        <input type="email" className="form-control"  placeholder="Enter email" required/>
+                        <label for="email" className="form-label md:block">Email:</label>
+                        <input type="email" className="form-control appearance-none md:rounded-md
+                        rounded-sm border border-gray-300 p-2 w-full hover:border-none
+                        focus:outline-none"  
+                        placeholder="Enter email" required/>
                     </div>
                     <div className="mb-3">
                         <label for="pwd" className="form-label">Password:</label>
-                        <input type="password" className="form-control" placeholder="Enter password" required/>
+                        <input type="password" className="form-control appearance-none 
+                        md:rounded-md focus:outline-none
+                        rounded-sm border border-gray-300 p-2 w-full hover:border-none" 
+                        placeholder="Enter password" required/>
                     </div>
                     <div className="mb-3">
                         <Link to="/forgot-password" className='text-decoration-none' 
@@ -30,17 +41,19 @@ function LogIn() {
                     </div>
                     <div className="form-check mb-3">
                         <label className="form-check-label">
-                            <input className="form-check-input" type="checkbox" name="remember"/>
+                            <input className="form-check-input" type="checkbox" 
+                            name="remember"/>
                             Remember me
                         </label>
                     </div>
 
                     <div className="d-grid mb-3">
-                        <button type="submit" className="btn btn-primary btn-block">
+                        <button type="submit" className="btn btn-primary btn-block
+                        w-full bg-blue-700 p-2 md:rounded-md rounded-sm text-white 
+                        font-bold">
                             Log In Now
                         </button>
                     </div>
-                    
                 </form>
             </div>
         </div>
