37e149d5af55c2e3a8b22605a43c6352a3c116a1
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-05-06 21:27:22 +0100

    added notificaiton icon to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faBell, faHospital } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
@@ -7,24 +7,29 @@ function Navbar() {
     return (
     <div className=''>
         <div className="container-xxl position-relative p-0 shadow-sm">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark
             px-4 px-lg-5 py-3 py-lg-0">
-                <NavLink to='/' className='navbar-brand p-3'>
-                    <h2 className="text-primary">
+                <NavLink to='/' className='navbar-brand p-3 space-x-3 text-white'>
+                    <FontAwesomeIcon icon={faHospital} 
+                    className='px-lg-3 px-0'/>
                         D'Clinik
-                    </h2>
                 </NavLink>
 
-                <button className='navbar-toggler'
+                <button className='navbar-toggler text-white'
                 type='button'
                 data-bs-toggle="collapse"
                 data-bs-target="#navbarCollapse">
-                    <FontAwesomeIcon icon={ faBars } />
+                    <FontAwesomeIcon icon={ faBars }
+                    className="text-white" />
 
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <div className="navbar-nav ms-auto py-0 px-3 text-start">
+                        <NavLink to="/"
+                        className="nav-item nav-link text-white">
+                            <FontAwesomeIcon icon={ faBell } />
+                        </NavLink>
                     </div>
                 </div>
             </nav>
