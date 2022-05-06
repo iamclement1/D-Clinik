e76aabb1d49b46d746e111c7f6667c71b8984398
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-05-06 18:30:07 +0100

    revamp login form

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Login from './Pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Navbar/>
-      <h1>
-        Welcome to D'Clinik
-      </h1>
+      <Login />
     </div>
   )
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { useState } from 'react'
+import { faStethoscope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Form } from 'react-bootstrap'
 
 function Login() {
+
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
     return (
-    <div>
+    <div className="">
+        <div className="container-xxl position-relative">
+            <div className="m-auto justify-center">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="flex offset-md-3 bg-white shadow-sm m-auto mt-12">
+                            <form className="mx-auto">
+                                <FontAwesomeIcon icon={faStethoscope} className='text-2xl'/>
+                                <h2 className='fs-4'>
+                                    Login to your account
+                                </h2>
+
+                                <div className='text-start'>
+                                    <label htmlFor="">Email</label>
+                                    <input type="email" className="form-control" 
+                                    placeholder="Email Address"
+                                    name="email" value={email}
+                                    onChange={ (e) => setEmail(e.target.value)} />
+                                </div>
+
+                                <div className='text-start'>
+                                    <label htmlFor="">Password</label>
+                                    <input type="password" className="form-control" 
+                                    placeholder="Password"
+                                    name='password'
+                                    value={password}
+                                    onChange={ (e) => setPassword(e.target.value)} />
+                                </div>
+
+                                <button className="btn btn-primary" type="submit"> Login</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="col-md-8"></div>
+                </div>
+            </div>
+        </div>
 
     </div>
 )
