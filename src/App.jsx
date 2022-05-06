2a0befe7471bb43090b941410b3196660e33804a
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-05-06 15:23:39 +0100

    revamped navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Navbar/>
       <h1>
         Welcome to D'Clinik
       </h1>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
     return (
-    <div>
-        
+    <div className='row'>
+        <div className="container-xxl position-relative p-0 shadow-sm">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light
+            px-4 px-lg-5 py-3 py-lg-0">
+                <NavLink to='/' className='navbar-brand p-3'>
+                    <h2 className="text-primary">
+                        D'Clinik
+                    </h2>
+                </NavLink>
+            </nav>
+        </div>
     </div>
   )
 }
