6f155d516abc53634dc4b860d0ead8c988e99e8c
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-05-07 15:57:33 +0100

    doctor dashboard

--- src/Pages/Doctor/Dashaboard.jsx
+++ src/Pages/Doctor/Dashaboard.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Dashaboard() {
     return (
     <div>
-        
+        <div className="container-xxl position-relative">
+            
+        </div>
     </div>
 
     )
