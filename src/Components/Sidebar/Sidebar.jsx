2c99f7d4d3ac3a976f1857c52ad193e6123bfa76
akanniquadree <78854242+akanniquadree@users.noreply.github.com>
2022-05-10 11:17:02 +0100

    Appointment Page and Sidebar added

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./sidebar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDashboard, faMailBulk } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+
+export default function Sidebar({sideOne, sideOneIcon}) {
+  return (
+    <div className="sidebar">
+        <div className="sidebarWrapper">
+            <ul className="sidebarWrapperList">
+                
+                <li className="sidebarWrapperListItem active">
+                    <FontAwesomeIcon icon={faDashboard} className="sidebarWrapperListItemIcon"/>  Dashboard
+                </li>
+                <Link to ="/doctor/appointment">
+                    <li className="sidebarWrapperListItem">
+                        <FontAwesomeIcon icon={sideOneIcon} className="sidebarWrapperListItemIcon"/> {sideOne}
+                    </li>
+                </Link>
+                
+                <li className="sidebarWrapperListItem">
+                    <FontAwesomeIcon icon={sideOneIcon} className="sidebarWrapperListItemIcon"/> {sideOne}
+                </li>
+                <li className="sidebarWrapperListItem">
+                    <FontAwesomeIcon icon={sideOneIcon} className="sidebarWrapperListItemIcon"/> {sideOne}
+                </li>
+                <li className="sidebarWrapperListItem">
+                    <FontAwesomeIcon icon={sideOneIcon} className="sidebarWrapperListItemIcon"/> {sideOne}
+                </li>
+                <li className="sidebarWrapperListItem">
+                    <FontAwesomeIcon icon={sideOneIcon} className="sidebarWrapperListItemIcon"/> {sideOne}
+                </li>
+                <li className="sidebarWrapperListItem">
+                    <FontAwesomeIcon icon={faMailBulk} className="sidebarWrapperListItemIcon"/> Mail
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
